48c87d0540c4c312c3a79c81b668f2ce90a951bb
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-26 14:18:42 -0300

    Estilizanbdo a dachboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
-import {Title} from './styles'
+import { FiChevronRight } from 'react-icons/fi';
+import { Title , Form, Repositories } from './styles'
+import logoimg from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () =>{
-  return <Title>Explore repositórios no Github</Title>
+  return (
+    <>
+      <img src={logoimg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53868812?v=4"
+            alt="Murillo Romeu"
+          />
+          <div>
+            <strong>Repositorio Murillo</strong>
+            <p>Descrição do meu repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53868812?v=4"
+            alt="Murillo Romeu"
+          />
+          <div>
+            <strong>Repositorio Murillo</strong>
+            <p>Descrição do meu repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53868812?v=4"
+            alt="Murillo Romeu"
+          />
+          <div>
+            <strong>Repositorio Murillo</strong>
+            <p>Descrição do meu repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 };
 
 export default Dashboard;
